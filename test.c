6da5d68e67dcef00c233aa2756e74f8c63b21c26
omidmirzajani <omidmirzajani82@gmail.com>
2021-07-09 00:50:07 +0430

    All

---
+++ test.c
@@ -0,0 +1,39 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "param.h"
+
+int main(int argc, const char *argv[])
+{
+
+    const int pcount = atoi(argv[1]);
+    int len;
+    int pids[pcount];
+    int i;
+    struct proc_info *procs = malloc(sizeof(struct proc_info) * NPROC);
+
+    for (i = 0; i < pcount; ++i)
+    {
+        int f = fork();
+        if (f==0)
+        {
+            malloc(getpid() * 11228 + 78);
+            while(1==1){}
+            exit();
+        }
+    }
+
+    sleep(100);
+
+    procs = malloc(sizeof(struct proc_info) * NPROC);
+    proc_dump(procs, &len);
+    printf(1, "pid\tsize\n");
+
+    for (i = 0; i < len; ++i)
+        printf(1, "%d\t%d\n", procs[i].pid, procs[i].memsize);
+
+    free(procs);
+
+    exit();
+    return 0;
+}
